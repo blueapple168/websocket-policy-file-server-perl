34385038b1ce92582c8df268d51a13638baa31a9
blueapple168 <blueapple1120@qq.com>
2018-11-15 08:58:43 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 LOGFILE=/workspace/flash_socket_policy.log
-exec /workspace/socketpolicy/socketpolicy.pl > /dev/null &
-
-if [ -f "$LOGFILE" ];then
-  tail -f "$LOGFILE"
-fi
+exec /workspace/socketpolicy/socketpolicy.pl > /dev/null & \
+     tail -f "$LOGFILE"
 
